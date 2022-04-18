e7b41c67ba8e822208b2006e5fac7de7475379ce
silviotmalmeida <silviotmalmeida@gmail.com>
2022-04-18 17:57:10 -0300

    Implementacao ate aula 109

--- app/app_super_gestao/app/SiteContact.php
+++ app/app_super_gestao/app/SiteContact.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContact extends Model
 {
-    //
+    //definindo o nome da tabela no BD
+    protected $table = 'site_contacts';
+
+    // definindo os atributos a serem informados
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'reason',
+        'message'
+    ];
 }
